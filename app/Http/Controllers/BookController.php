8b7cb05898c1e0020fe267b3a7bfa964d3bdf765
Vicu Gabriel-Alexandru <gabi.vicu@gmail.com>
2023-02-22 18:59:24 +0200

    CRUD methods for book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -28,6 +28,9 @@ class BookController extends Controller
      */
     public function index()
     {
+        $books = Book::all();
+
+        return $this->successResponse($books);
     }
 
     /**
@@ -36,6 +39,18 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
+        $rules = [
+            'title' => 'required|max:255',
+            'description' => 'required|max:255',
+            'price' => 'required|min:1',
+            'author_id' => 'required|min:1'
+        ];
+
+        $this->validate($request, $rules);
+
+        $book = Book::create($request->all());
+
+        return $this->successResponse($book, Response::HTTP_CREATED);
 
     }
     
@@ -45,7 +60,9 @@ class BookController extends Controller
      */
     public function show($book)
     {
+        $book = Book::findOrFail($book);
 
+        return $this->successResponse($book);
     }
 
         /**
@@ -54,6 +71,26 @@ class BookController extends Controller
      */
     public function update(Request $request, $book)
     {
+        $rules = [
+            'title' => 'max:255',
+            'description' => 'max:255',
+            'price' => 'min:1',
+            'author_id' => 'min:1'
+        ];
+
+        $this->validate($request, $rules);
+
+        $book = Book::findOrFail($book);
+
+        $book->fill($request->all());
+
+        if ($book->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $book->save();
+
+        return $this->successResponse($book);
 
     }
 
@@ -63,5 +100,10 @@ class BookController extends Controller
      */
     public function destroy($book)
     {
+        $book = Book::findOrFail($book);
+
+        $book->delete();
+
+        return $this->successResponse($book);
     }
 }
